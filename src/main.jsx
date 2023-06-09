d71f5c0756f178bd64d0c6cd64e548411c6f7e55
nik <nnaiker01@gmail.com>
2023-06-09 21:25:19 +1200

    removed about page

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./pages/About.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/about",
-    element: <About />,
   },
   {
     path: "*",
--- src/pages/About.jsx
+++
@@ -1,5 +0,0 @@
-function About(){
-    return <h1>hi</h1>
-}
-
-export default About
